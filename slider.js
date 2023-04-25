ee86a38576557684361e4c7d2fd9c856edbe0b54
Bea Pan <bpankowska@gmail.com>
2023-04-25 22:06:32 +0200

    just tidy-up

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 let slider_img = document.querySelector('.slider-img');
-let images = ['CSS-codecademy.jpg', 'HTML-codecademy.jpg', 'JavaScript-UDEMY.jpg', 'JavaScript-sololearn.png'];
+let images = ['HTML-codecademy.jpg','CSS-codecademy.jpg', 'JavaScript-UDEMY.jpg', 'JavaScript-sololearn.png'];
 let i = 0; //current image index
 
 function prev(){
